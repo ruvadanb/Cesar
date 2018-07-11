9b5dc14a25a66701dc55211602d380650c6896b4
Daniel Ruvalcaba Barboza <39530003+ruvadanb@users.noreply.github.com>
2018-07-11 10:53:47 -0500

    Update Tarea_5.c

--- Tarea_5.c
+++ Tarea_5.c
@@ -2,32 +2,34 @@
 #include <stdlib.h>
 #include "Tarea_5.h"
 
-struct strNode{
+struct strVertex{
 	unsigned id;
 	Type Data;
 	List Vertex;
 };
 
-typedef struct strNode *Vertex;
+typedef struct strVertex *Vertex;
 
 struct strGraph{
-	Vertex *vertex;	//Arreglo de vértices, número de nodos en el grafo
-	unsigned Size; //número de aristas
-	unsigned Order; //número de vértices
+	Vertex *vertex;	//Arreglo de vÃ©rtices, nÃºmero de nodos en el grafo
+	unsigned Size; //nÃºmero de aristas
+	unsigned Order; //nÃºmero de vÃ©rtices
+	unsigned limit;//Control de arreglo de vertex
 	Print myPrint; //Funciones asociadas
 	CMP myCmp;
 };
+
+unsigned int indice=1;
 
 Graph graph_create(CMP comparator, Print print){
 	Graph new = (Graph)malloc(sizeof(struct strGraph));
 	if(!new) return NULL;
 	new->myCmp = comparator;
 	new->myPrint = print;
-	new->getIndex = index;
 	new->Order = 0;
 	new->Size = 0;
 	new->vertex = NULL;
-	//new->vertex = (Vertex*)calloc(new->limit,sizeof(Vertex));
+	new->vertex = (Vertex*)malloc(sizeof(Vertex));
 	return new;
 }
 
@@ -35,7 +37,7 @@ void graph_destroy(Graph g){
 	Vertex temp;
 	unsigned start = 0;
 	if(g){
-		while(start < g->limit){
+		while(start < g->Order){
 			temp = g->vertex[start];
 			if(temp){
 				if(temp->Vertex) list_destroy(temp->Vertex);
@@ -46,7 +48,6 @@ void graph_destroy(Graph g){
 		free(g);
 	}
 }
-
 /*void graph_newSize(Graph g){
 	if (g){
 		unsigned newSize = (g->limit)*1.2, i;
@@ -65,36 +66,43 @@ void graph_destroy(Graph g){
 	}
 }*/
 
-boolean graph_addVertex(Graph g, Type data){
-	if(!g) return false;
-	unsigned id = g->getIndex(data,g->limit);
-	if(g->Order == 0){
-		g->vertex = (Vertex*)calloc(g->limit,sizeof(Vertex));
+Vertex newVertex(Type data, unsigned int* contador){
+	Vertex new=(Vertex)malloc(sizeof(Vertex));
+	if(new!=NULL){
+		new->Vertex=list_create();
+		new->id=*contador;
+		new->Data=data;
 	}
-	while(g->vertex[id]){
-		if(g->myCmp(g->vertex[id]->Data,data)==0) return false;
-		id ++ ;
-		if(id == g->limit) id -= g->limit;
-	}
-	Vertex new = (Vertex)malloc(sizeof(struct strNode));
-	if(!new) return false;
-	new->Data = data;
-	new->Vertex = list_create();
-	new->id = id;
-	g->vertex[id]=new;
-	g->Order++;
-	return true;
+	(*contador)++;
+	return new;
 }
 
-boolean graph_addEdge(Graph g, Type source, Type sink){
-	if(!g) return false;
-	unsigned id;
-	id = g->getIndex(source, g->limit);
-	while(g->myCmp(g->vertex[id]->Data, source) != 0){
-		id++;
-		if(id==g->limit)id -= g->limit;
+boolean graph_addVertex(Graph g, Type data){
+	if(g->Order==0){
+		g->vertex[g->Order]=newVertex(data, &indice);
+		g->Order++;
+		return true;
+	} else {
+		g->vertex = (Vertex*)realloc(g->vertex,sizeof(Vertex)*(g->Order+1));
+		g->vertex[g->Order]=newVertex(data, &indice);
+		g->Order++;
+		return true;
 	}
-	if(list_add(g->vertex[id]->Vertex, sink)){
+	return false;
+}
+boolean graph_addEdge(Graph g, Type source, Type sink){
+	if(!g)
+		return false;
+	int contador=0, destino=0;
+	while(g->myCmp(g->vertex[contador]->Data, source) != 0&&contador<g->Order){
+		contador++;
+	}
+	while(g->myCmp(g->vertex[destino]->Data, sink)!=0&&destino<g->Order){
+		destino++;
+	}
+	if(contador==g->Order||destino==g->Order)
+		return false;
+	if(list_add(g->vertex[contador]->Vertex, g->vertex[destino])){
 		g->Size++;
 		return true;
 	}
@@ -112,44 +120,39 @@ unsigned long graph_edgeCount(Graph g){
 }
 
 unsigned long graph_outDegree(Graph g, Type source){
-	Vertex temp;
-	unsigned id;
+	unsigned verti=0;
 	if(g){
-		id = g->getIndex(source, g->limit);
-		temp = g->vertex[id];
-		while(g->myCmp(source, temp->Data)!=0){
-			id++;
-			if(id == g->limit) id -= g->limit;
-			temp = g->vertex[id];
+		while(g->myCmp(g->vertex[verti]->Data, source)!=0&&verti<g->Order){
+			verti++;
 		}
-		return list_size(temp->Vertex);
+		if(verti==g->Order)
+			return -1;
+		else
+			return list_size(g->vertex[verti]->Vertex);
 	}
 	return -1;
 }
 
 boolean graph_hasEdge(Graph g, Type source, Type sink){
-	if(!g)return false;
-	unsigned id = g->getIndex(source,g->limit);
-	while(g->myCmp(g->vertex[id]->Data, source)!= 0){
-		id++;
-		if(id == g->limit) id -= g->limit;
-	}
-	List list_Vertex = g->vertex[id]->Vertex;
-	unsigned i = 1;
-	Type aux;
-	while(i<=list_size(list_Vertex)){
-		aux = list_get(list_Vertex,i);
-		if(g->myCmp(aux,sink) == 0) return true;
-		i++;
+	int inicio=0, destino=0;
+	while(g->myCmp(g->vertex[inicio]->Data, source)!=0&&inicio<g->Order)
+		inicio++;
+	if(inicio!=g->Order){
+		while(g->myCmp(list_get(g->vertex[inicio]->Vertex, destino), sink)!=0&&destino<list_size(g->vertex[inicio]->Vertex)){
+			destino++;
+		}
+		if(destino<list_size(g->vertex[inicio]->Vertex)){
+			return true;
+		}
 	}
 	return false;
 }
 
-void graph_print(Graph g, Print p){
+void graph_print(Graph g){
 	Vertex temp;
 	unsigned id = 0;
 	if(g){
-		while(id < g->limit){
+		while(id < g->Order){
 			temp = g->vertex[id];
 			if(temp)g->myPrint(temp->Data);
 			id++;
