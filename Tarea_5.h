b71f24e744dce453b2f980c006f1c80c5556b3b2
Daniel Ruvalcaba Barboza <39530003+ruvadanb@users.noreply.github.com>
2018-07-11 10:58:49 -0500

    Update Tarea_5.h

--- Tarea_5.h
+++ Tarea_5.h
@@ -7,7 +7,7 @@ typedef struct strGraph *Graph;
 typedef void (*Print) (Type);
 typedef int (*CMP)(Type, Type);
 
-Graph graph_create(CMP comparator, Print print, Index index);
+Graph graph_create(CMP comparator, Print print);
 void graph_destroy(Graph g);
 boolean graph_addVertex(Graph g, Type data);
 boolean graph_addEdge(Graph g, Type source, Type skin);
@@ -15,6 +15,6 @@ unsigned long graph_vertexCount(Graph g);
 unsigned long graph_edgeCount(Graph g);
 unsigned long graph_outDegree(Graph g, Type source);
 boolean graph_hasEdge(Graph g, Type source, Type skin);
-void graph_print(Graph g, Print p);
+void graph_print(Graph g);
 
 #endif /* GRAPH_H_ */
